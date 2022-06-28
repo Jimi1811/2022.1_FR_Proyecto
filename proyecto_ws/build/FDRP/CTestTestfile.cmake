28840fc40bb003424dcbacecaa7f8f8bc1b7a989
Jimi1811 <jim.fabian@utec.edu.pe>
2022-06-28 01:29:36 -0500

    1ra version en URDF

---
+++ proyecto_ws/build/FDRP/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jim/FR_Proyecto_2022-1/proyecto_ws/src/FDRP
+# Build directory: /home/jim/FR_Proyecto_2022-1/proyecto_ws/build/FDRP
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ proyecto_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jim/FR_Proyecto_2022-1/proyecto_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/home/jim/lab_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/home/jim/lab_ws/install/lib'
+export PWD='/home/jim/FR_Proyecto_2022-1/proyecto_ws/build'
+export PYTHONPATH='/home/jim/lab_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:/home/jim/lab_ws/install/lib/python2.7/site-packages'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jim/FR_Proyecto_2022-1/proyecto_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jim/FR_Proyecto_2022-1/proyecto_ws/src:$ROS_PACKAGE_PATH"
