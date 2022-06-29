48bdd4b99a9be251a69dda22a76ae04e0e92f08c
Jimi1811 <jim.fabian@utec.edu.pe>
2022-06-29 03:45:44 +0000

    catkin_make hecho

---
+++ project_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/FR_Proyecto_2022-1/project_ws/src
+# Build directory: /home/user/FR_Proyecto_2022-1/project_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(VL)
---
+++ project_ws/build/VL/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/FR_Proyecto_2022-1/project_ws/src/VL
+# Build directory: /home/user/FR_Proyecto_2022-1/project_ws/build/VL
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
